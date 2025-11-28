7196984af93ae560e0df03cf30e654efb2529079
Niki Ramandika <nikiramandika@icloud.com>
2025-11-28 19:27:41 +0700

    trs

--- departemen_controller.py
+++ departemen_controller.py
@@ -4,9 +4,9 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ipv4
+from ryu.lib.packet import ethernet, arp, ipv4
 from ryu.lib.packet import ether_types
+from ipaddress import ip_network, ip_address
 
 
 class DeptFirewall(app_manager.RyuApp):
@@ -14,55 +14,46 @@ class DeptFirewall(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(DeptFirewall, self).__init__(*args, **kwargs)
+
+        # MAC learning per switch
         self.mac_to_port = {}
 
-        # Firewall rules (berdasarkan prefix IP)
-        # Format: ("src_prefix", "dst_prefix")
+        # Firewall rules
         self.block_rules = [
-            ("192.168.10.240/28", "192.168.10.96/27"),   # Mahasiswa → Pimpinan G9 L2
-            ("192.168.10.240/28", "192.168.10.160/27"),  # Mahasiswa → Lab
-            ("172.16.21.0/28", "172.16.21.32/27"),       # Mahasiswa G10 L1 → Dosen L3
-            ("172.16.21.16/28", "172.16.21.32/27"),      # Mahasiswa G10 L2 → Dosen L3
+            ("192.168.10.240/28", "192.168.10.96/27"),   # Mhs → Pimpinan
+            ("192.168.10.240/28", "192.168.10.160/27"),  # Mhs → Lab
+            ("172.16.21.0/28", "172.16.21.32/27"),       # G10 L1 → Dosen
+            ("172.16.21.16/28", "172.16.21.32/27"),      # G10 L2 → Dosen
         ]
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS,
+            actions
+        )]
+
+        mod = parser.OFPFlowMod(datapath=datapath,
+                                priority=priority,
+                                match=match,
+                                instructions=inst)
+        datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         parser = datapath.ofproto_parser
-        ofproto = datapath.ofproto
 
+        # Default rule: always send unknown packets to controller
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
-
+        actions = [parser.OFPActionOutput(datapath.ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 0, match, actions)
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        parser = datapath.ofproto_parser
-        ofproto = datapath.ofproto
-
-        inst = [parser.OFPInstructionActions(
-            ofproto.OFPIT_APPLY_ACTIONS, actions)]
-
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath,
-                                    buffer_id=buffer_id,
-                                    priority=priority,
-                                    match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath,
-                                    priority=priority,
-                                    match=match,
-                                    instructions=inst)
-        datapath.send_msg(mod)
-
     def check_block(self, src_ip, dst_ip):
-        from ipaddress import ip_network, ip_address
-
-        for src_net, dst_net in self.block_rules:
-            if ip_address(src_ip) in ip_network(src_net) and \
-               ip_address(dst_ip) in ip_network(dst_net):
+        for s, d in self.block_rules:
+            if ip_address(src_ip) in ip_network(s) and ip_address(dst_ip) in ip_network(d):
                 return True
         return False
 
@@ -73,46 +64,59 @@ class DeptFirewall(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         ofproto = datapath.ofproto
 
-        in_port = msg.match['in_port']
-
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
-
-        dst = eth.dst
-        src = eth.src
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        # Learning mac address
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        in_port = msg.match["in_port"]
+
+        src = eth.src
+        dst = eth.dst
+
+        # MAC learning
         self.mac_to_port[dpid][src] = in_port
 
-        # Check IPv4 untuk firewall
-        ip_pkt = pkt.get_protocol(ipv4.ipv4)
-        if ip_pkt:
-            src_ip = ip_pkt.src
-            dst_ip = ip_pkt.dst
+        # ====== ARP HANDLING (fix ARP not reachable) ======
+        arp_pkt = pkt.get_protocol(arp.arp)
+        if arp_pkt:
+            out_port = ofproto.OFPP_FLOOD
+            actions = [parser.OFPActionOutput(out_port)]
 
-            # Check blocking
+            # No flow install; always broadcast ARP
+            out = parser.OFPPacketOut(
+                datapath=datapath,
+                buffer_id=msg.buffer_id,
+                in_port=in_port,
+                actions=actions,
+                data=msg.data
+            )
+            datapath.send_msg(out)
+            return
+
+        # IPv4 – firewall check
+        ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ipv4_pkt:
+            src_ip = ipv4_pkt.src
+            dst_ip = ipv4_pkt.dst
+
             if self.check_block(src_ip, dst_ip):
-                self.logger.info("FIREWALL: BLOCK %s → %s", src_ip, dst_ip)
-                return  # Paket dibuang
+                self.logger.info("BLOCKED %s → %s", src_ip, dst_ip)
+                return
+            else:
+                self.logger.info("ALLOW %s → %s", src_ip, dst_ip)
 
-        # Normal switching
+        # NORMAL SWITCHING
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
-            out_port = ofproto.OFPP_FLOOD  # Agar satu gedung tetap reachable
+            out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
 
-        if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-            self.add_flow(datapath, 1, msg.match, actions, msg.buffer_id)
-            return
-        else:
-            self.add_flow(datapath, 1, msg.match, actions)
+        # Install flow for learned destination
+        match = parser.OFPMatch(eth_src=src, eth_dst=dst)
+        self.add_flow(datapath, 10, match, actions)
 
         out = parser.OFPPacketOut(datapath=datapath,
                                   buffer_id=msg.buffer_id,
